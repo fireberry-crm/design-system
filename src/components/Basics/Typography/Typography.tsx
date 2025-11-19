04c72b8bcdf5b8e664a309b82697c88e13c7ac3c
Niv <niv.itzikovich@fireberry.com>
2025-11-19 12:06:09 +0200

    fix: as compoennt

--- src/components/Basics/Typography/Typography.tsx
+++ src/components/Basics/Typography/Typography.tsx
@@ -16,6 +16,8 @@ const Typography: FC<TypographyProps> = ({
   const { fontSize, lineHeight, fontWeight = 400 } = theme.typography[type];
   const textColor = theme.typography.colors[color];
 
+  const htmlElement = HTML_ELEMENTS?.[type as keyof typeof HTML_ELEMENTS] || 'span';
+
   return (
     <S.Text
       fontSize={fontSize}
@@ -25,6 +27,7 @@ const Typography: FC<TypographyProps> = ({
       bold={bold}
       underline={underline}
       ellipsis={ellipsis}
+      as={htmlElement}
     >
       {children}
     </S.Text>
@@ -32,3 +35,5 @@ const Typography: FC<TypographyProps> = ({
 };
 
 export default Typography;
+ 
+const HTML_ELEMENTS= {h1:'h1',h2:'h2',h3:'h3'} as const;
