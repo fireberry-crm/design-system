034d01d68542949e692d05aab5902b8a93e1c849
Niv <niv.itzikovich@fireberry.com>
2025-11-23 12:12:42 +0200

    fix: change max-height to 100% and not 1000px

--- src/components/Basics/Collapse/style.ts
+++ src/components/Basics/Collapse/style.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const CollapseContainer = styled.div<{ isOpen: boolean }>`
   overflow: hidden;
-  max-height: ${({ isOpen }) => (isOpen ? '1000px' : '0')};
+  max-height: ${({ isOpen }) => (isOpen ? '100%' : '0')};
   opacity: ${({ isOpen }) => (isOpen ? 1 : 0)};
   transition: max-height 0.3s ease, opacity 0.3s ease;
 `;
